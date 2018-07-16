1eaaaa8b6810fb1ae8ccb1d625c4814aa6053ec2
Emma Di Battista <emmadibattista@gmail.com>
2018-07-16 11:51:18 -0400

    there

--- digit.erl
+++ digit.erl
@@ -1,19 +1,22 @@
 -module(digit).
 -compile(export_all).
 
+
+%%%% PROXY %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 proxy() ->
     process_flag(trap_exit, true),
-	Self = self(),
-	register(proxy, Self),
-	{ok, ListenSocket} = gen_tcp:listen(12345, [{active,true},binary]),
-	spawn(fun() -> clienthandler(Self, ListenSocket) end),
-	proxy_loop([],0).
+    Self = self(),
+    register(proxy, Self),
+    {ok, ListenSocket} = gen_tcp:listen(12345, [{active,true},binary]),
+    spawn(fun() -> clienthandler(Self, ListenSocket) end),
+    proxy_loop([],0).
 
 
 proxy_loop(PIDList, Coord) ->
     receive
-		{From, client, Data} ->
-			io:fwrite("~p client said ~p~n",[From, Data]);
+        {From, client, Data} ->
+            io:fwrite("~p client said ~p~n",[From, Data]),
+            Coord ! {self(), {From, client, Data}};
 
         {From, new_server} ->
             NewPIDList = [From | PIDList],
@@ -56,24 +59,24 @@ proxy_loop(PIDList, Coord) ->
     proxy_loop(PIDList, Coord).
 
 clienthandler(Master, ListenSocket) ->
-	io:fwrite("clienthandler started~n"),
-	{ok, Socket} = gen_tcp:accept(ListenSocket),
-	io:fwrite("socket bound~n"),
-	spawn(fun() -> clienthandler(Master, ListenSocket) end),
-	clienthandlerloop(Socket, Master).
+    io:fwrite("clienthandler started~n"),
+    {ok, Socket} = gen_tcp:accept(ListenSocket),
+    io:fwrite("socket bound~n"),
+    spawn(fun() -> clienthandler(Master, ListenSocket) end),
+    clienthandlerloop(Socket, Master).
 
 clienthandlerloop(Socket, Master) ->
-	receive
-		{tcp, Socket, Data} ->
-			io:fwrite("~p ~p~n",[Socket, Data]),
-			Master ! {self(), client, Data},
-			gen_tcp:send(Socket,[<<"ok">>]);
-		Weird ->
-			io:fwrite("received unexpected message: ~p~n",[Weird])
-	end,
-	clienthandlerloop(Socket, Master).
+    receive
+        {tcp, Socket, Data} ->
+            io:fwrite("~p ~p~n",[Socket, Data]),
+            Master ! {self(), client, Data},
+            gen_tcp:send(Socket,[<<"ok">>]);
+        Weird ->
+            io:fwrite("received unexpected message: ~p~n",[Weird])
+    end,
+    clienthandlerloop(Socket, Master).
 
-
+%%%% SERVER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 server(Proxy_node) ->
     {proxy, Proxy_node} ! {self(), new_server},%{{{
     process_flag(trap_exit, true),
@@ -84,7 +87,7 @@ server(Proxy_node) ->
             server_loop(PIDList, Coord, Proxy);
         Other ->
             io:format("~n((~p))Received:~n~p~n",[self(), Other])
-    end.%}}}
+    end.
 
 server_loop(PIDList, Coord, Proxy) ->
     receive
@@ -115,48 +118,62 @@ server_loop(PIDList, Coord, Proxy) ->
             io:format("~n((~p))Server ~p died~n~p", [self(), Server, NewPIDList]),
 
             server_loop(NewPIDList, Coord, Proxy);
+        
+        {Proxy, {Client, client, Data}} ->
+            [Command, Filename] = re:split(Data, "#"),
+            io:fwrite("~p says do ~p ~p~n", [Client, Command, Filename]);
+            % case Command of
+            %     <<"commit">> ->
+            %         io:fwrite("~p says do ~p ~p~n", [Client, Command, Filename]);
+            %     <<"update">> ->
+            %         io:fwrite("~p says do ~p ~p~n", [Client, Command, Filename]);
+            %     <<"checkout">> ->
+            %         io:fwrite("~p says do ~p ~p~n", [Client, Command, Filename])
+            % end;
+
         Other ->
-            io:format("~n((~p))Received:~n~p~n",[self(),Other])
-    end.
+            io:format("~n((~p))Received:~n~p~n",[self(),Other])    
+    end,
+    server_loop(PIDList, Coord, Proxy).
 
 
 
 %%%% CLIENT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 client() ->
-	{_, Socket} = gen_tcp:connect({127,0,0,1}, 12345, [binary, {active,true}]),
-	client_loop(Socket).
+    {_, Socket} = gen_tcp:connect({127,0,0,1}, 12345, [binary, {active,true}]),
+    client_loop(Socket).
 
 client_loop(Socket) ->
-	{ok, [Command, Filename]} = io:fread("[vega] > ","~a ~s"),
-		case Command of
-			commit when Filename =/= [] ->
-				gen_tcp:send(Socket,["commit ", Filename]);
+    {ok, [Command, Filename]} = io:fread("[vega] > ","~a ~s"),
+        case Command of
+            commit when Filename =/= [] ->
+                gen_tcp:send(Socket,["commit#", Filename]);
 
-			checkout when Filename =/= [] ->
-				gen_tcp:send(Socket,["checkout ", Filename]);
+            checkout when Filename =/= [] ->
+                gen_tcp:send(Socket,["checkout#", Filename]);
 
-			update when Filename =/= [] ->
-				gen_tcp:send(Socket,["update ", Filename]);
+            update when Filename =/= [] ->
+                gen_tcp:send(Socket,["update#", Filename]);
 
-			_ ->
-				io:fwrite("Invalid expression. Use:~n"),
-				io:fwrite("	commit [filename]~n"),
-				io:fwrite("	checkout [filename]~n"),
-				io:fwrite("	update [filename]~n"),
-				client_loop(Socket)
-		end,
-	receive
-		{tcp, Socket, Data} ->
-			io:fwrite("~p ~s~n",[Socket, Data]);
+            _ ->
+                io:fwrite("Invalid expression. Use:~n"),
+                io:fwrite(" commit [filename]~n"),
+                io:fwrite(" checkout [filename]~n"),
+                io:fwrite(" update [filename]~n"),
+                client_loop(Socket)
+        end,
+    receive
+        {tcp, Socket, Data} ->
+            io:fwrite("~p ~s~n",[Socket, Data]);
 
-		Message ->
-			Message
+        Message ->
+            Message
 
-	end,
-	client_loop(Socket).
+    end,
+    client_loop(Socket).
 
 
-
+%%%% OTHER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 multicast(PIDList, Msg) ->
     Ref = erlang:make_ref(),
     lists:map (fun(PID) -> PID ! {self(), Ref, Msg} end, PIDList),
